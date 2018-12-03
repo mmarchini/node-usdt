5baa91785fb0f67e446c454bb335f1cf4549e166
Matheus Marchini <mat@mmarchini.me>
2018-12-03 15:30:56 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! reword! switch to n-api

--- usdt-provider.cc
+++ usdt-provider.cc
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "usdt.h"
 
 namespace usdt {
@@ -27,7 +29,7 @@ USDTProvider::USDTProvider(const CallbackInfo& args)
 }
 
 USDTProvider::~USDTProvider() {
-  providerDestroy(provider);
+  providerDestroy(provider_);
 }
 
 Object USDTProvider::Init(Napi::Env env, Object exports) {
@@ -36,9 +38,9 @@ Object USDTProvider::Init(Napi::Env env, Object exports) {
   Function func = DefineClass(
       env, "USDTProvider",
       {
-          InstanceMethod("addProbe", &USDTProvider::addProbe),
-          InstanceMethod("enable", &USDTProvider::enable),
-          InstanceMethod("disable", &USDTProvider::disable),
+          InstanceMethod("addProbe", &USDTProvider::AddProbe),
+          InstanceMethod("enable", &USDTProvider::Enable),
+          InstanceMethod("disable", &USDTProvider::Disable),
       });
 
   constructor = Persistent(func);
@@ -50,81 +52,82 @@ Object USDTProvider::Init(Napi::Env env, Object exports) {
 
 Value USDTProvider::AddProbe(const CallbackInfo& args) {
   std::string probe_name(args[0].As<String>());
-  USDTProbe probe = USDTProbe::constructor.New({});
+  Object probe_obj = USDTProbe::constructor.New({});
+  USDTProbe* probe = ObjectWrap<USDTProbe>::Unwrap(probe_obj);
 
   this->Set(probe_name, probe);
 
   // TODO(mmarchini) validate args len <= MAX_ARGUMENTS
-  probe_.argc = std::min(MAX_ARGUMENTS, args.Length());
-  for (int i = 0; i < probe_.argc; i++) {
+  probe->argc = std::min(MAX_ARGUMENTS, args.Length());
+  for (int i = 0; i < probe->argc; i++) {
     std::string type = args[i + 1].As<String>();
 
     switch {
       case "char *":
-        probe_.arguments[i] = uint64;
+        probe->arguments[i] = uint64;
         break;
       case "int":
-        probe_.arguments[i] = int32;
+        probe->arguments[i] = int32;
         break;
       default:
-        probe_.arguments[i] = uint64;
+        probe->arguments[i] = uint64;
         break;
     }
   }
 
-  switch (probe_.argc) {
+  switch (probe->argc) {
     case 6:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0],
-        probe_.arguments[1],
-        probe_.arguments[2],
-        probe_.arguments[3],
-        probe_.arguments[4],
-        probe_.arguments[5]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0],
+        probe->arguments[1],
+        probe->arguments[2],
+        probe->arguments[3],
+        probe->arguments[4],
+        probe->arguments[5]
       );
       break;
     case 5:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0],
-        probe_.arguments[1],
-        probe_.arguments[2],
-        probe_.arguments[3],
-        probe_.arguments[4]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0],
+        probe->arguments[1],
+        probe->arguments[2],
+        probe->arguments[3],
+        probe->arguments[4]
       );
       break;
     case 4:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0],
-        probe_.arguments[1],
-        probe_.arguments[2],
-        probe_.arguments[3]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0],
+        probe->arguments[1],
+        probe->arguments[2],
+        probe->arguments[3]
       );
       break;
     case 3:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0],
-        probe_.arguments[1],
-        probe_.arguments[2]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0],
+        probe->arguments[1],
+        probe->arguments[2]
       );
       break;
     case 2:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0],
-        probe_.arguments[1]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0],
+        probe->arguments[1]
       );
       break;
     case 1:
-      probe_.probe = providerAddProbe(provider_, probe_name, probe_.argc,
-        probe_.arguments[0]
+      probe->probe = providerAddProbe(provider_, probe_name, probe->argc,
+        probe->arguments[0]
       );
       break;
     case 0:
     default:
-      probe_.probe = providerAddProbe(provider->provider, *name, probe_.argc);
+      probe->probe = providerAddProbe(provider->provider, *name, probe->argc);
       break;
   }
 
-  return probe;
+  return probe_obj;
 }
 
 Value USDTProvider::Enable(const CallbackInfo& args) {
