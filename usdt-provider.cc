940175ad441a7864fc5e64e4b192f2b6385aefda
Matheus Marchini <mat@mmarchini.me>
2018-12-03 15:39:49 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! reword! switch to n-api

--- usdt-provider.cc
+++ usdt-provider.cc
@@ -139,6 +139,7 @@ Value USDTProvider::Enable(const CallbackInfo& args) {
 }
 
 Value USDTProvider::Disable(const CallbackInfo& args) {
+  Napi::Env env = args.Env();
   if (providerUnload(provider_) != 0) {
     // TODO (mmarchini) throw error
     // ThrowError("Unable to unload provider");
